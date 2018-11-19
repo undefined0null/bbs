c78636d17dcc574944dafa219ac6cdfaddd4a44f
Cai miao <caimiao2017@outlook.com>
2018-11-19 12:05:35 +0000

    站点管理

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://github.com/undefined0null/bbs" target="_blank">Caimiao</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:caimiao2017@outlook.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
