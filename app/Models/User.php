9ed6725e43a65f100c0b0de3288a3165609efd06
Cai miao <caimiao2017@outlook.com>
2018-11-19 05:59:57 +0000

    安装laravel-permission扩展包

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 use Auth;
 
 class User extends Authenticatable
@@ -11,6 +12,8 @@ class User extends Authenticatable
     use Notifiable {
         notify as protected laravelNotify;
     }
+
+    use HasRoles;
 
     public function notify($instance)
     {
