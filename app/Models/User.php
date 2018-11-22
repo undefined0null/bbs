dafd1669343adafd42b33ca89b23fed74989262e
Cai miao <caimiao2017@outlook.com>
2018-11-22 05:20:35 +0000

    第三方OAUTH授权登录API

--- app/Models/User.php
+++ app/Models/User.php
@@ -35,6 +35,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'email', 'password', 'introduction', 'avatar', 'phone',
+        'weixin_openid', 'weixin_unionid'
     ];
 
     /**
