2e1884022355ae4ae8cbeaa00fce927a98906e2c
Cai miao <caimiao2017@outlook.com>
2018-11-29 09:49:05 +0000

    标记未读通知API

--- routes/api.php
+++ routes/api.php
@@ -102,6 +102,9 @@ $api->version('v1', [
             // 通知统计
             $api->get('user/notifications/stats', 'NotificationsController@stats')
                 ->name('api.user.notifications.stats');
+            // 标记通知已读
+            $api->patch('user/read/notifications', 'NotificationsController@read')
+                ->name('api.user.notifications.read');
         });
     });
 });
